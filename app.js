9301b92a516ba86c36aac5dbc2a0a0a89fe5ff7d
williamtran207 <quochuyphbrvt07@gmail.com>
2023-12-11 20:55:53 +0700

    update

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ const cors = require('cors')
 const OpenAI = require('openai')
 
 const openai = new OpenAI({
-    apiKey: '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'
+    apiKey: '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'
 })
 
 app.use(cors())
 
 app.use(async (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    
     let messages = req.query.ques;
 
     const response = await openai.chat.completions.create({
@@ -28,6 +24,10 @@ app.use(async (req, res, next) => {
         frequency_penalty: 0,
         presence_penalty: 0,
     });
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 
     res.status(200).json(response)
 })
